fce019b6e82dff27809af0b25ac831b71e640a98
Vanessa Chau <vanessaschau@gmail.com>
2020-05-24 12:35:06 -0700

    Initial Commit

---
+++ FriendsOnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FriendsOnTheMap
+//
+//  Created by Vanessa on 5/24/20.
+//  Copyright © 2020 Vanessa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
